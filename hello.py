fcb1eb547d51e8749b3a611694cf06ad54e24506
fkxodls <47289970+fkxodls@users.noreply.github.com>
2019-10-06 15:09:32 +0900

    sdsd
    sdsd

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
 hour = input("enter hour")
 rate = input("enter rate per hour")
-if float(hour) <= 40.0:
-    pay = float(hour)*float(rate)
-elif float(hour) > 40.0:
-    pay = 40.0 * float(rate) + (float(hour) - 40.0)*1.5*float(rate)
-print("pay:", pay)
+hour = float(hour)
+rate = float(rate)
+
+def computepay(hour, rate):
+    if hour <= 40 :
+        pay = hour * rate
+        return pay
+    else :
+        pay = rate*(1.5*hour-20)
+        return pay
+  
+print(computepay(hour,rate))
+    
