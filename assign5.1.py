d5b37445bc57ca8f8dd890f3c43283a9c372c0ce
fkxodls <47289970+fkxodls@users.noreply.github.com>
2019-10-12 14:16:56 +0900

    5.1
    5.1

--- assign5.1.py
+++ assign5.1.py
@@ -1,1 +1,30 @@
+lnum = None
+snum = None
 
+while True:
+    num = input("enter a number ")
+
+    if num == 'done':
+        break 
+    
+    try:
+        num = int(num)
+
+    except:
+        print('invalid value')
+        continue
+
+    if  lnum == None:
+        lnum = num
+        snum = num
+
+    elif num > lnum:
+        lnum = num
+
+    elif num < snum:
+        snum = num   
+
+
+
+print("Maximum is",lnum)
+print("Minimum is",snum)
